57af46ba368db627ec86bb4a99259c5cf9b54512
rohitpatil2992001@gmail.com <rohitpatil2992001@gmail.com>
2024-06-23 22:17:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 // src/App.js
 
-import './App.css';
-import Dashboard from './component/dashboard';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
+import Navbar from './component/navbar/navbar';
+import EventRegistrationForm from './component/eventRegistrationForm';
+import JobApplicationForm from './component/jobApplicationForm';
+import "./App.css";
+import SurveyForm from './component/surveyForm';
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar/>
+        <div className="flex-1 p-8">
+          <Routes>
+          <Route path="/" element={<Navigate to="/registration" replace />} />
+          <Route path="/registration"  element={<EventRegistrationForm/>} />
+          <Route path="/job-application" element={<JobApplicationForm/>} />
+          <Route path="/survey-form" element={<SurveyForm/>} />
+          </Routes>
+            
+        </div>
+      </div>
+    </Router>
   );
 }
 
